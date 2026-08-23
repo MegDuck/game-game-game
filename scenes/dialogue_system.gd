930ebdcbe683a84b0ad31a06bb0764dd38edc6fb
MegDuck <vsyakayavsyachina1234@gmail.com>
2026-08-23 21:47:06 +0800

    dialogue system v3

--- scenes/dialogue_system.gd
+++ scenes/dialogue_system.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var main_body: RichTextLabel = $MainBody
+@onready var main_body: RichTextLabel = $VBoxContainer/Dialogue/MainBody
 @export var typing_speed: float = 0.02
 
 
@@ -12,27 +12,31 @@ func _ready() -> void:
 # This is the function that handles the Array of Strings
 func _on_display_text(text_lines: Array[String], character_id: String) -> void:
 	# Process each string in the array one by one
-	for txt in text_lines:
-		print(txt)
-		
-		var formatted_text = txt
-		if character_id != "narrator" and character_id != "":
-			formatted_text = "[" + character_id + "] " + txt
-			
-		if main_body.text != "":
-			formatted_text = "\n\n" + formatted_text
-			
-		main_body.append_text(formatted_text)
-		
-		var start_chars = main_body.visible_characters
-		var end_chars = main_body.get_total_character_count()
-		
-		main_body.visible_characters = start_chars
-		
-		var tween = create_tween()
-		tween.tween_property(main_body, "visible_characters", end_chars, (end_chars - start_chars) * typing_speed)
-		
-		await tween.finished
+	
+	main_body.text = text_lines[0]
+	print(main_body.text)
+	
+	#for txt in text_lines:
+		#print(txt)
+		#
+		#var formatted_text = txt
+		#if character_id != "narrator" and character_id != "":
+			#formatted_text = "[" + character_id + "] " + txt
+			#
+		#if main_body.text != "":
+			#formatted_text = "\n\n" + formatted_text
+			#
+		#main_body.append_text(formatted_text)
+		#
+		#var start_chars = main_body.visible_characters
+		#var end_chars = main_body.get_total_character_count()
+		#
+		#main_body.visible_characters = start_chars
+		#
+		#var tween = create_tween()
+		#tween.tween_property(main_body, "visible_characters", end_chars, (end_chars - start_chars) * typing_speed)
+		#
+		#await tween.finished
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
